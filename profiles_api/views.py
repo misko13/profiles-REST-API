42a5b220738d6e270da95932b139d7b8ecba10eb
Dragan <misolic.dragan@gmail.com>
2024-02-14 11:47:31 +0100

    added hello apiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View define application logic extending a base class APIView"""
+
+    def get(self, request, format=None):
+        """Returns a LIST of APIView Features - HTTP get requests"""    
+        an_apiview =[
+            'Uses HTTP method as fiunction (get, post, patch, put, delete)',
+            'Is very similar as traditional Django View',
+            'Gives You the most controll over you apploication logic',
+            'Is mapped to URLs',
+        ]
+        """ This is a sample list, we look tyo get a response OBJECT converted to JSON  - it must be a list or a dictionary"""
+        """We pass bellow the dictionary"""
+
+        return Response({'messagge': 'Hello World', 'an_apiview': an_apiview  })
